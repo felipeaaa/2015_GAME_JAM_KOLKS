f14fee8ca5f17169769ef09a0ac69f0f07120834
felipeaaa <felipeaa-10@hotmail.com>
2015-04-25 10:47:33 -0300

    Movimentacao50porcento

--- Source/Assets/Scripts/Movimentacao.cs
+++ Source/Assets/Scripts/Movimentacao.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Movimentacao : MonoBehaviour {
 	public KeyCode right, left, jump;
+	float tempo;
 	Rigidbody2D playerFisica;
 	public Transform testePulo1, testePulo2, testePulo3;
 	public Transform testeDireita1, testeDireita2, testeDireita3;
@@ -14,11 +15,12 @@ public class Movimentacao : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(right)){
-			playerFisica.velocity = new Vector2(5, playerFisica.velocity.y);
+		tempo += Time.deltaTime;
+		if(Input.GetKey(right) && tempo >0.5f ){
+			playerFisica.velocity = new Vector2(8, playerFisica.velocity.y);
 		}
-		if(Input.GetKey(left)){
-			playerFisica.velocity = new Vector2(-5, playerFisica.velocity.y);
+		if(Input.GetKey(left) && tempo >0.5f ){
+			playerFisica.velocity = new Vector2(-8, playerFisica.velocity.y);
 		}
 		pular();
 		PularParaDireita();
@@ -41,9 +43,9 @@ public class Movimentacao : MonoBehaviour {
 	}
 
 	void pular(){
-	  	RaycastHit2D hit1 =	Physics2D.Raycast(testePulo1.position, -Vector2.up, 0.2f);
-		RaycastHit2D hit2 =	Physics2D.Raycast(testePulo2.position, -Vector2.up, 0.2f);
-		RaycastHit2D hit3 =	Physics2D.Raycast(testePulo3.position, -Vector2.up, 0.2f);
+	  	RaycastHit2D hit1 =	Physics2D.Raycast(testePulo1.position, -Vector2.up, 0.1f);
+		RaycastHit2D hit2 =	Physics2D.Raycast(testePulo2.position, -Vector2.up, 0.1f);
+		RaycastHit2D hit3 =	Physics2D.Raycast(testePulo3.position, -Vector2.up, 0.1f);
 
 
 
@@ -51,31 +53,34 @@ public class Movimentacao : MonoBehaviour {
 
 		if((hit1.collider != null) || (hit1.collider != null) || (hit1.collider != null)){
 			if(Input.GetKeyDown(jump)){
-				playerFisica.velocity = new Vector2( playerFisica.velocity.x,10);
+				playerFisica.velocity = new Vector2( playerFisica.velocity.x,6);
 			}
 		}
 
 	}
 	void PularParaDireita(){
-		RaycastHit2D ParedeE1 =	Physics2D.Raycast(testeEsquerda1.position, -Vector2.right, 0.2f);
-		RaycastHit2D ParedeE2 =	Physics2D.Raycast(testeEsquerda2.position, -Vector2.right, 0.2f);
-		RaycastHit2D ParedeE3=	Physics2D.Raycast(testeEsquerda3.position, -Vector2.right, 0.2f);
+		RaycastHit2D ParedeE1 =	Physics2D.Raycast(testeEsquerda1.position, -Vector2.right, 0.1f);
+		RaycastHit2D ParedeE2 =	Physics2D.Raycast(testeEsquerda2.position, -Vector2.right, 0.1f);
+		RaycastHit2D ParedeE3=	Physics2D.Raycast(testeEsquerda3.position, -Vector2.right, 0.1f);
 
 		if((ParedeE1.collider != null) || (ParedeE2.collider != null) || (ParedeE3.collider != null)){
 			if(Input.GetKeyDown(jump)){
-				playerFisica.velocity = new Vector2( -10,10);
+				tempo = 0;
+				playerFisica.velocity = new Vector2( 6,6);
+
 			}
 		}
 	}
 	void PularParaEsquerda(){
 		
-		RaycastHit2D ParedeD1 =	Physics2D.Raycast(testeDireita1.position, Vector2.right, 0.2f);
-		RaycastHit2D ParedeD2 =	Physics2D.Raycast(testeDireita2.position, Vector2.right, 0.2f);
-		RaycastHit2D ParedeD3=	Physics2D.Raycast(testeDireita3.position, Vector2.right, 0.2f);
+		RaycastHit2D ParedeD1 =	Physics2D.Raycast(testeDireita1.position, Vector2.right, 0.1f);
+		RaycastHit2D ParedeD2 =	Physics2D.Raycast(testeDireita2.position, Vector2.right, 0.1f);
+		RaycastHit2D ParedeD3=	Physics2D.Raycast(testeDireita3.position, Vector2.right, 0.1f);
 
 		if((ParedeD1.collider != null) || (ParedeD2.collider != null) || (ParedeD3.collider != null)){
 			if(Input.GetKeyDown(jump)){
-				playerFisica.velocity = new Vector2( 10,10);
+				tempo = 0;
+				playerFisica.velocity = new Vector2( -6,6);
 			}
 		}
 	}
